02071953ec5d548625bf8226348a779edb78a5e3
Shaho Rasooli <schaho.rasooli@gmail.com>
2025-12-27 08:45:50 +0000

    12 final

--- JavaScript/Supersimle.dev/scripts/12r.js
+++ JavaScript/Supersimle.dev/scripts/12r.js
@@ -5,41 +5,56 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 };
 
 updateScoreElement();
+
 /*
-      if (!score) {setInterval
-        score = {
-          wins: 0,
-          losses: 0,
-          ties: 0,
-        };
-      }
-      */
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+}
+*/
 
 let isAutoPlaying = false;
-let intervalID;
+let intervalId;
 
+//const autoPlay = () => {
+
+//};
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalID = setInterval(() => {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
     isAutoPlaying = true;
+
+    // Solution for 12t.
+    document.querySelector(".js-auto-play-button").innerHTML = "Stop Playing";
   } else {
-    clearInterval(intervalID);
+    clearInterval(intervalId);
     isAutoPlaying = false;
+
+    // Solution for 12t.
+    document.querySelector(".js-auto-play-button").innerHTML = "Auto Play";
   }
 }
 
-document.querySelector(".js-rock-button").addEventListener("click", () => {
+// Solution for exercise 12s.
+document.querySelector(".js-auto-play-button").addEventListener("click", () => {
+  autoPlay();
+});
+
+document.querySelector(".js-Rock-button").addEventListener("click", () => {
   playGame("Rock");
 });
 
-document.querySelector(".js-paper-button").addEventListener("click", () => {
+document.querySelector(".js-Paper-button").addEventListener("click", () => {
   playGame("Paper");
 });
 
-document.querySelector(".js-scissors-button").addEventListener("click", () => {
+document.querySelector(".js-Scissors-button").addEventListener("click", () => {
   playGame("Scissors");
 });
 
@@ -50,6 +65,18 @@ document.body.addEventListener("keydown", (event) => {
     playGame("Paper");
   } else if (event.key === "s") {
     playGame("Scissors");
+
+    // Solution for 12u.
+  } else if (event.key === "a") {
+    autoPlay();
+
+    // Solution for 12w.
+  } else if (event.key === "Backspace") {
+    // Solution for 12w.
+    // resetScore();
+
+    // Solution for 12x.
+    showResetConfirmation();
   }
 });
 
@@ -57,6 +84,7 @@ function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
   let result = "";
+
   if (playerMove === "Scissors") {
     if (computerMove === "Rock") {
       result = "You lose.";
@@ -87,7 +115,7 @@ function playGame(playerMove) {
     score.wins += 1;
   } else if (result === "You lose.") {
     score.losses += 1;
-  } else {
+  } else if (result === "Tie.") {
     score.ties += 1;
   }
 
@@ -98,9 +126,9 @@ function playGame(playerMove) {
   document.querySelector(".js-result").innerHTML = result;
 
   document.querySelector(".js-moves").innerHTML = `You
-      <img src="images/${playerMove}-emoji.png" class="move-icon" />
-      <img src="images/${computerMove}-emoji.png" class="move-icon" />
-      computer`;
+<img src="../../images/${playerMove}-emoji.png" class="move-icon">
+<img src="../../images/${computerMove}-emoji.png" class="move-icon">
+Computer`;
 }
 
 function updateScoreElement() {
@@ -111,7 +139,9 @@ function updateScoreElement() {
 
 function pickComputerMove() {
   const randomNumber = Math.random();
+
   let computerMove = "";
+
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = "Rock";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
@@ -121,4 +151,54 @@ function pickComputerMove() {
   }
 
   return computerMove;
+}
+
+// Solution for 12v.
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem("score");
+  updateScoreElement();
+}
+
+// Solution for 12v.
+document
+  .querySelector(".js-reset-score-button")
+  .addEventListener("click", () => {
+    // Solution for 12v.
+    // resetScore();
+
+    // Solution for 12x.
+    showResetConfirmation();
+  });
+
+// Solution for 12x.
+function showResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = `
+      Are you sure you want to reset the score?
+      <button class="js-reset-confirm-yes reset-confirm-button">
+        Yes
+      </button>
+      <button class="js-reset-confirm-no reset-confirm-button">
+        No
+      </button>
+    `;
+
+  document
+    .querySelector(".js-reset-confirm-yes")
+    .addEventListener("click", () => {
+      resetScore();
+      hideResetConfirmation();
+    });
+
+  document
+    .querySelector(".js-reset-confirm-no")
+    .addEventListener("click", () => {
+      hideResetConfirmation();
+    });
+}
+
+function hideResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = "";
 }
